06bd6aa482b385132d59bce6764e104acfa942a2
yowlik <yowl609@gmail.com>
2022-08-12 15:44:49 +0300

    test

---
+++ app/src/main/java/com/pocket/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.pocket.black
+
+object CNST {
+    const val AF_DEV_KEY = "dAspjCjfizaMJMYWihTe8Z"
+    const val jsoupCheck = "4g6g"
+    const val ONESIGNAL_APP_ID = "969cd86f-8130-4a56-a385-aa6de1913fd1"
+
+    var lru = "http://solarorchid.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
---
+++ app/src/main/java/com/pocket/white/Game.kt
@@ -0,0 +1,74 @@
+package com.pocket.white
+
+import android.content.Intent
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ImageView
+import com.pocket.R
+import com.pocket.databinding.ActivityGameBinding
+import java.nio.file.Files.find
+import kotlin.Result
+
+class Game : AppCompatActivity() {
+
+
+    val roll=mutableListOf<String>("1","2","3")
+    var scr=0
+
+
+    lateinit var binding:ActivityGameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.b1.setOnClickListener {
+            val i2:String ="1"
+            val i=roll.random()
+            binding.pl.setImageResource(R.drawable.ball)
+            check(i,binding.en)
+            if(i2==i){
+                scr++
+                binding.score.text="${scr}"
+            }
+
+        }
+        binding.b2.setOnClickListener {
+            val i2:String="2"
+            val i=roll.random()
+            binding.pl.setImageResource(R.drawable.ball1)
+            check(i,binding.en)
+            if(i2==i){
+                scr++
+                binding.score.text="${scr}"
+            }
+        }
+        binding.b3.setOnClickListener {
+            val i2:String="3"
+            binding.pl.setImageResource(R.drawable.ball2)
+            val i=roll.random()
+            check(i,binding.en)
+            if(i2==i){
+                scr++
+                binding.score.text="${scr}"
+            }
+        }
+
+
+
+    }
+
+    fun check(i:String,im: ImageView) {
+        if (i == "1") {
+            im.setImageResource(R.drawable.ball)
+        } else if (i == "2") {
+            im.setImageResource(R.drawable.ball1)
+
+        } else {
+            im.setImageResource(R.drawable.ball2)
+        }
+    }
+}
